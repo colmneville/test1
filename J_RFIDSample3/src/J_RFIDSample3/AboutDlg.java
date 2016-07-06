f857d472f35d38bc63e50e1b56bf36dbba44eba2
Colm Neville <colm.neville@in2sequence.com>
2016-07-06 16:42:33 +0100

    added a line change

--- J_RFIDSample3/src/J_RFIDSample3/AboutDlg.java
+++ J_RFIDSample3/src/J_RFIDSample3/AboutDlg.java
@@ -16,53 +16,55 @@ import com.mot.rfid.api3.*;
 
 public class AboutDlg {
 
-	private Shell sShell = null;  //  @jve:decl-index=0:visual-constraint="7,4"
-	private Button buttonOK = null;
-	private Label labelAppName = null;
-	private Label labelVersion = null;
-	private Label labelVersionInfo = null;
-	
-	AboutDlg()
-	{
-		createSShell();
-		sShell.open();
-	}
-	/**
-	 * This method initializes sShell
-	 */
-	private void createSShell() {
-		sShell = new Shell(SWT.CLOSE);
-		sShell.setText("About");
-		sShell.setLayout(null);
-		sShell.setSize(240, 188);//new Point(238, 147));
-		buttonOK = new Button(sShell, SWT.NONE);
-		buttonOK.setText("OK");
-		buttonOK.setBounds(new Rectangle(160, 92, 57, 23));
-		buttonOK.addSelectionListener(new org.eclipse.swt.events.SelectionAdapter() {
-			public void widgetSelected(org.eclipse.swt.events.SelectionEvent e) {
-				sShell.close();
-			}
-		});
-		labelAppName = new Label(sShell, SWT.NONE);
-		labelAppName.setBounds(new Rectangle(16, 18, 164, 19));
-		//labelAppName.setText("J_RFIDSample3");
-		labelVersion = new Label(sShell, SWT.NONE);
-		labelVersion.setBounds(new Rectangle(16, 49, 100, 13));
-		labelVersion.setText("RFID C DLL Version");
-		labelVersionInfo = new Label(sShell, SWT.NONE);
-		labelVersionInfo.setBounds(new Rectangle(122, 49, 106, 13));
-		labelVersionInfo.setText("Label");
-		
-		labelAppName.setText(RFIDMainDlg.rfidBase.APP_NAME);
-		
-	    String versionInfo = "Unknown";
+  private Shell sShell = null;  //  @jve:decl-index=0:visual-constraint="7,4"
+  private Button buttonOK = null;
+  private Label labelAppName = null;
+  private Label labelVersion = null;
+  private Label labelVersionInfo = null;
+
+
+  // Added a comment here
+  AboutDlg()
+  {
+    createSShell();
+    sShell.open();
+  }
+  /**
+   * This method initializes sShell
+   */
+  private void createSShell() {
+    sShell = new Shell(SWT.CLOSE);
+    sShell.setText("About");
+    sShell.setLayout(null);
+    sShell.setSize(240, 188);//new Point(238, 147));
+    buttonOK = new Button(sShell, SWT.NONE);
+    buttonOK.setText("OK");
+    buttonOK.setBounds(new Rectangle(160, 92, 57, 23));
+    buttonOK.addSelectionListener(new org.eclipse.swt.events.SelectionAdapter() {
+      public void widgetSelected(org.eclipse.swt.events.SelectionEvent e) {
+        sShell.close();
+      }
+    });
+    labelAppName = new Label(sShell, SWT.NONE);
+    labelAppName.setBounds(new Rectangle(16, 18, 164, 19));
+    //labelAppName.setText("J_RFIDSample3");
+    labelVersion = new Label(sShell, SWT.NONE);
+    labelVersion.setBounds(new Rectangle(16, 49, 100, 13));
+    labelVersion.setText("RFID C DLL Version");
+    labelVersionInfo = new Label(sShell, SWT.NONE);
+    labelVersionInfo.setBounds(new Rectangle(122, 49, 106, 13));
+    labelVersionInfo.setText("Label");
+
+    labelAppName.setText(RFIDMainDlg.rfidBase.APP_NAME);
+
+      String versionInfo = "Unknown";
         try {
-        	versionInfo = RFIDMainDlg.rfidBase.getMyReader().versionInfo().getVersion();
-   
+          versionInfo = RFIDMainDlg.rfidBase.getMyReader().versionInfo().getVersion();
+
         } catch (InvalidUsageException ex) {
         } catch (OperationFailureException ex) {
         }
         labelVersionInfo.setText(versionInfo);
-;	}
+; }
 
 }
